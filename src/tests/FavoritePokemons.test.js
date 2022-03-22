5f39a982524e0c2773ffc69a1f9c7dfb4ead760d
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-03-22 20:12:37 -0300

    Enviando o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './rederWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const favoritePokemons = pokemons.filter((_poke, index) => index < 2);
+
+describe('Testes referentes ao componente FavoritePokemons.js', () => {
+  it(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+  se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+
+    const noFavoritesMessage = screen.getByText('No favorite pokemon found');
+
+    expect(noFavoritesMessage).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+
+    const pokemon = screen.getAllByTestId('pokemon-name');
+
+    expect(pokemon).toHaveLength(2);
+  });
+});
